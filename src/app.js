8369b32a9bcebd0504e515a287307c01767970f7
Mohamed Essam <mohamedessam.moe@gmail.com>
2021-05-13 17:07:42 +0200

    get 100 trending repos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const root = require("./routes/index");
+const repos = require("./routes/repos");
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,7 @@ app.use(express.json());
 const port = process.env.PORT;
 
 app.use(root);
+app.use(repos);
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
---
+++ src/routes/repos.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const router = express.Router();
+
+router.get("/repos", async (req, res) => {
+  const api_url =
+    "https://api.github.com/search/repositories?q=created:>30&sort=stars&order=desc&per_page=100";
+  const response = await fetch(api_url);
+  const data = await response.json();
+
+  var urls = data.items
+    .map((a) => ({
+      language: a.language,
+      url: a.url,
+    }))
+    .filter((el) => {
+      return el.language != null;
+    });
+
+  result = Object.entries(
+    urls.reduce((r, { language, url }) => {
+      if (!r[language]) r[language] = [];
+      r[language].push(url);
+      return r;
+    }, {})
+  ).map(([language, urls]) => ({ language, urls, count: urls.length }));
+
+  res.send(result);
+});
+
+module.exports = router;
